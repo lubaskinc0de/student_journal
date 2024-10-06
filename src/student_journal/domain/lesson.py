1e5047f714afc00dd1d71c2a91145ef9a51c6c54
lubaskin <lubaskincorporation@gmail.com>
2024-10-06 11:15:07 +0300

    add lesson_id to home task

--- src/student_journal/domain/lesson.py
+++ src/student_journal/domain/lesson.py
@@ -3,7 +3,6 @@ from datetime import datetime
 
 from student_journal.domain.value_object.lesson_id import LessonId
 from student_journal.domain.value_object.subject_id import SubjectId
-from student_journal.domain.value_object.task_id import TaskId
 
 
 # урок
@@ -14,6 +13,5 @@ class Lesson:
     at: datetime  # когда урок пройдет?
     mark: int | None  # оценка за урок
     note: str | None  # записка
-    task_id: TaskId | None  # домашнее задание
     room: int  # кабинет
     index_number: int  # номер урока
