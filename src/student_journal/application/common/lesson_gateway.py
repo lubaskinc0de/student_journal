c389edc9760bd2951931bcf96de39460a3868f3f
RigalHD <profoundromeo@gmail.com>
2024-10-06 12:50:52 +0300

    added Lesson gateway, lesson exceptions, lesson create, read

---
+++ src/student_journal/application/common/lesson_gateway.py
@@ -0,0 +1,19 @@
+from abc import abstractmethod
+from typing import Protocol
+
+from student_journal.domain.lesson import Lesson
+from student_journal.domain.value_object.lesson_id import LessonId
+
+
+class LessonGateway(Protocol):
+    @abstractmethod
+    def read_lesson(self, lesson_id: LessonId) -> Lesson: ...
+
+    @abstractmethod
+    def write_lesson(self, lesson: Lesson) -> LessonId: ...
+
+    @abstractmethod
+    def update_lesson(self, lesson: Lesson, lesson_id: LessonId) -> LessonId: ...
+
+    @abstractmethod
+    def delete_lesson(self, lesson_id: LessonId) -> None: ...
