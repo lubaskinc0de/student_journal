02c1b11e059f81e5348c918ddb8fa1fa926b3156
RigalHD <profoundromeo@gmail.com>
2024-10-07 19:11:30 +0300

    added HomeTask creation and home task invariants validator

---
+++ src/student_journal/hometask/create_home_task.py
@@ -0,0 +1,44 @@
+from dataclasses import dataclass
+from uuid import uuid4
+
+from student_journal.application.common.home_task_gateway import HomeTaskGateway
+from student_journal.application.common.transaction_manager import TransactionManager
+from student_journal.application.invariants.home_task import (
+    validate_home_task_invariants,
+)
+from student_journal.domain.home_task import HomeTask
+from student_journal.domain.value_object.lesson_id import LessonId
+from student_journal.domain.value_object.task_id import TaskId
+
+
+@dataclass(slots=True, frozen=True)
+class NewHomeTask:
+    lesson_id: LessonId
+    description: str
+    is_done: bool = False
+
+
+@dataclass(slots=True)
+class CreateHomeTask:
+    gateway: HomeTaskGateway
+    transaction_manager: TransactionManager
+
+    def execute(self, data: NewHomeTask) -> TaskId:
+        self.transaction_manager.begin()
+
+        validate_home_task_invariants(
+            description=data.description,
+        )
+
+        task_id = TaskId(uuid4())
+        lesson = HomeTask(
+            task_id=task_id,
+            lesson_id=data.lesson_id,
+            description=data.description,
+            is_done=data.is_done,
+        )
+
+        self.gateway.write_home_task(lesson)
+        self.transaction_manager.commit()
+
+        return task_id
