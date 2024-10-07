359e8c1ffa21327217450c7db59db8f7748c3ba8
RigalHD <profoundromeo@gmail.com>
2024-10-07 19:16:24 +0300

    added HomeTask read

---
+++ src/student_journal/hometask/delete_home_task.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+
+from student_journal.application.common.home_task_gateway import HomeTaskGateway
+from student_journal.application.common.transaction_manager import TransactionManager
+from student_journal.domain.value_object.task_id import TaskId
+
+
+@dataclass(slots=True)
+class DeleteHomeTask:
+    transaction_manager: TransactionManager
+    gateway: HomeTaskGateway
+
+    def execute(self, task_id: TaskId) -> None:
+        self.transaction_manager.begin()
+        self.gateway.delete_home_task(task_id)
+        self.transaction_manager.commit()
