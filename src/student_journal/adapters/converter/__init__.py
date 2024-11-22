ef07260ecafd8fd04b169f5a412d37937aa4532e
lubaskin <lubaskincorporation@gmail.com>
2024-11-22 22:28:12 +0300

    bump ruff

--- src/student_journal/adapters/converter/__init__.py
+++ src/student_journal/adapters/converter/__init__.py
@@ -4,13 +4,13 @@ from .student import student_retort, student_to_list_retort
 from .teacher import teacher_retort, teacher_to_list_retort
 
 __all__ = [
-    "teacher_retort",
-    "teacher_to_list_retort",
-    "student_retort",
-    "student_to_list_retort",
+    "home_task_retort",
+    "home_task_to_list_retort",
     "lesson_retort",
     "lesson_to_list_retort",
     "lessons_to_list_retort",
-    "home_task_retort",
-    "home_task_to_list_retort",
+    "student_retort",
+    "student_to_list_retort",
+    "teacher_retort",
+    "teacher_to_list_retort",
 ]
